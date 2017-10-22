116d772a68014fb6d53a0f8706830859634d4d6a
PikleRick <ybendagh@localhost.localdomain>
2017-10-22 18:54:51 +0200

    testResult : no feasible solution found :/

--- Projet1/colonneF.m
+++ Projet1/colonneF.m
@@ -15,16 +15,30 @@ f = zeros(p*c*d*t,1);
 % cette notation à pour but de faciliter l'implémentation
 % Remarque : de cette façon on incremente d'abord i, puis j, et ensuite k1 (les creneaux, puis les jours de la semaine)
 
-% n = i1 + (j1-1)*p + (l1-1)*p*c
-for i1 = 1 : p
-	for j1 = 1 : c
-		for l1 = 1 : d
-			n1 = i1 + (j1-1)*p + ((l-1)*t + 1)*p*c;
-			n2 = i1 + (j1-1)*p + ((l-1)*t + t)*p*c;
-
-			f(n1,1) = 1;
-			f(n2,1) = 1;
-            
+% m = i + (j-1)*p + (k-1)*p*c + (l-1)*p*c*t;
+for i = 1 : p
+	for j = 1 : c
+		for l = 1 : d
+            for k = 1 : t
+                m = i + (j-1)*p + (k-1)*p*c + (l-1)*p*c*t;
+                if (k == 1) || (k == t)
+                    f(m) = 1;
+                end
+            end
         end
 	end
 end
+
+lb = zeros(p*c*d*t,1);
+ub = ones(p*c*d*t,1);
+intcon = ones(p*c*d*t,1);
+% m = i + (j-1) * p + (k-1) * p * c
+for i = 1 : p
+	for j = 1 : c
+		for k = 1 : (d*t)
+    		m = i + ((j - 1) * p) + ((k - 1) * p * c);
+			intcon(m,1) = m;
+		end
+	end
+end
+
