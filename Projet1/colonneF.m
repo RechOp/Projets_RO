790b3320f593635f4994322570bb160a4685fd59
PikleRick <youssef.professionnel@gmail.com>
2017-10-26 19:44:00 +0200

    p1

--- Projet1/colonneF.m
+++ Projet1/colonneF.m
@@ -1,8 +1,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% la matrice f (en fait c une colonne)
+% f est la fonction (vecteur) à minimiser
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % edt RechOp
+
 p = 8;
 c = 2;
 d = 5;
@@ -10,12 +11,7 @@ t = 4;
 
 f = zeros(p*c*d*t,1);
 
-% les indices de f sont 'n' et '1', tel que : {n = i + (j-1)*p + (l-1)*p*c }
-% avec i, j et k1 = ((l-1)*t + k) les 3 indices de la matrice tridimensionnelle X (ramenée à une seule colonne de taille p*c*d*t (pour faciliter l'mplementation du problème),
-% cette notation à pour but de faciliter l'implémentation
-% Remarque : de cette façon on incremente d'abord i, puis j, et ensuite k1 (les creneaux, puis les jours de la semaine)
 
-% m = i + (j-1)*p + (k-1)*p*c + (l-1)*p*c*t;
 for i = 1 : p
 	for j = 1 : c
 		for l = 0 : (d - 1)
@@ -25,6 +21,10 @@ for i = 1 : p
 	end
 end
 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% on cherche x telle que (lb <= x <= ub); intcon contient les indices de x
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 lb = zeros(p*c*d*t,1);
 ub = ones(p*c*d*t,1);
 intcon=1:p*c*dt;
