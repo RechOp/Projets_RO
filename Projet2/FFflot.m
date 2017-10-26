05c0996677f1ac3af5b9edb326475fea32a38548
rxambili <32817521+rxambili@users.noreply.github.com>
2017-10-26 23:41:01 +0200

    Tentative non testé

--- Projet2/FFflot.m
+++ Projet2/FFflot.m
@@ -12,74 +12,76 @@
 %
 %% Initialisations
 NSUC = uint16([1 2 2 1]);
+NPRE = uint16([a remplir]);
 n = size(NSUC,2);
 X = uint16(1:n); % X contient la liste des sommets de G
 m = sum(NSUC);
 %
 % Vecteur des successeurs de chaque sommet : SUC
 SUC = uint16([2 3 4 1 4 1]);
+% Vecteur des successeurs de chaque sommet : PRE
+PRE = uint16([a remplir]);
 %
 % Vecteur des capacités max de chaque arc
 CAPMAX = [inf 1 4 5 2 3];
 CAP = [0 0 0 0 0 0];
 %
-% Vecteur du flot courant phi
-phi = 0;
+
 %
 MARQUE = false(1,n); % MARQUE est un vecteur logique
 MARQUE(2) = true; % on marque le sommet a == 2 ( b == 1 (b,a) == 1)
 %
-%MARQUES = X(MARQUE); % NONMARQUES contient la liste des sommets non marqués
+
 %
 
 
 %% Algorithme de FF
 
-bloque =false; 
-i = 2; % i est marqué
-while ismember(1,NONMARQUES) && ~bloque % Tant que b == 1 non marqué
+bloque = false; 
 
-    %% 1.   MAJ flot courant theta
-    alpha = inf;
-    vcycle = zeros(1,m); % vecteur cycle des sommets non marqués
-    if NSUC(i) ~= 0 % le nombre de successeurs de i est non nul
-        prsuc = sum(NSUC(1:i-1)) + 1; % prsuc contient l'indice du 1er successeur de i dans SUC
-        k = prsuc;
-        j = SUC(k);
-        while(~MARQUE(j) && k <= prsuc + NSUC(i)-1)
-            %for k = prsuc:prsuc+NSUC(i)-1
-            j = SUC(k);
-            if CAP(i + k - prsuc) < CAPMAX(i + k - prsuc)
-                MARQUE(j)= true;
-                
-            end
-            k=k+1;
-        end
-        if (k>  prsuc + NSUC(i)-1)
-            bloque = true;
-        else
-            i=j;
-        end
-    end
+while ~bloque % Tant que chemin possible 
+
+    %% 1.   Trouver chemin
+	chemin = trouverChemin(2,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, 1);
+	
+	%% 2. Mettre à jour les capicités des flots
+    if chemin == -1
+		bloque = true;
+	else
+		alpha = inf;
+		ind_cap=[];
+		% on parcourt des noeuds par lesquels le chemin passe
+		for i = 1:(length(chemin)-1)
+			noeud = chemin(i);
+			succ = chemin(i+1);
+			
+			trouve = false;
+			prsuc = sum(NSUC(1:noeud-1)) + 1; % prsuc contient l'indice du 1er successeur de i dans SUC
+			k = prsuc;
+			while ~trouve & k<=prsuc + NSUC(noeud)-1  % on parcours les successeurs
+				j = SUC(k);
+				if (j == succ) % le flot est dans le bon sens
+					trouve = true;
+					ind_cap(i) = i + k - prsuc;
+					alpha = min(alpha, CAPMAX(ind_cap(i)));
+				end
+				k = k+1;
+			end
+			prpre = sum(NPRE(1:noeud-1)) + 1; % prpre contient l'indice du 1er predecesseur de i dans PRE
+			k = prpre;
+			while ~trouve & k<=prpre + NPRE(noeud)-1  % on parcours les predecesseurs
+				j = PRE(k);
+				if (j == succ) % le flot est dans le bon sens
+					trouve = true;
+					ind_cap(i) = i + k - prpre;
+					alpha = min(alpha, CAP(ind_cap(i)));
+				end
+				k = k+1;
+			end
+		end
+		% On augmente les flots
+		CAP(ind_cap) = CAP(ind_cap) + alpha;
+		CAP(1) = CAP(1) + alpha;
+	end
 end
-    theta = theta + alpha*vcycle; %MAJ theta
-    %
-    %% 2.   Marquer sommets
-    liste_candidats_marquage = X(CANDIDATS);
-    for l=1:size(liste_candidats_marquage,2)
-        i = liste_candidats_marquage(l); % i est non marqué et candidat à être marqué
-        if NSUC(i) ~= 0
-            % le nombre de successeurs de i est non nul
-            prsuc = sum(NSUC(1:i-1)) + 1; % prsuc contient l'indice du 1er successeur de i dans SUC
-            for k = prsuc:prsuc+NSUC(i)-1
-                j = SUC(k); % j est successeur du sommet i candidat au marquage
-                if MARQUE(j) && theta(k) < LONG(k)
-                    % i est non marqué , j est marqué, (i,j) est un arc (de numéro k) 
-                    % donc on marque i
-                    MARQUE(i) = true; % on marque le sommet i
-                    NONMARQUES = setdiff(NONMARQUES,i); % on enlève i des sommets non marqués
-                end
-            end
-        end
-    end
-end
+
---
+++ Projet2/trouverChemin.m
@@ -0,0 +1,43 @@
+function chemin = trouverChemin(i,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, p)
+	if NSUC(i) ~= 0 % le nombre de successeurs de i est non nul
+        prsuc = sum(NSUC(1:i-1)) + 1; % prsuc contient l'indice du 1er successeur de i dans SUC
+        for k=prsuc:prsuc + NSUC(i)-1  % on parcours les successeurs
+            j = SUC(k);
+			if ~MARQUE(j)
+				if CAP(i + k - prsuc) < CAPMAX(i + k - prsuc) % Si flot non saturé
+					MARQUE(j)= true;
+					chemin(p)=j;
+					if j != 1
+						chemin = trouverChemin(j,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, p+1);
+						if chemin(size(chemin,2)) != 1 % Si on a pas atteint le bout
+							MARQUE(j) = false;
+							chemin = chemin(1:p-1);
+						end
+					end
+				end
+			end
+        end
+    end
+	if NPRE(i) ~= 0 % le nombre de predecesseurs de i est non nul
+        prpre = sum(NPRE(1:i-1)) + 1; % prpre contient l'indice du 1er predecesseur de i dans SUC
+        for k=prpre:prpre + NPRE(i)-1  % on parcours les predecesseur
+            j = PRE(k);
+			if ~MARQUE(j)
+				if CAP(i + k - prpre) > 0 % Si flot non saturé
+					MARQUE(j)= true;
+					chemin(p)=j;
+					if j != 1
+						chemin = trouverChemin(j,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, p+1);
+						if chemin(size(chemin,2)) != 1 % Si on a pas atteint le bout
+							MARQUE(j) = false;
+							chemin = chemin(1:p-1);
+						end
+					end
+				end
+			end
+        end
+    end
+	if chemin(size(chemin,2)) != 1 % Si on a pas atteint le bout
+		chemin = -1; % pas de chemin possible
+	end
+end
