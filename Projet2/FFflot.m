45024a4563c1684d385f8ace01303eb5af43782b
Xambili Robin <rxambili@localhost.localdomain>
2017-10-27 14:38:56 +0200

    FFlot.m OK

--- Projet2/FFflot.m
+++ Projet2/FFflot.m
@@ -18,12 +18,12 @@ X = uint16(1:n); % X contient la liste des sommets de G
 m = sum(NSUC);
 %
 % Vecteur des successeurs de chaque sommet : SUC
-SUC = uint16([2 3 4 1 4 1]);
+SUC = uint16([2 3 4 4 1 1]);
 % Vecteur des successeurs de chaque sommet : PRE
 PRE = uint16([3 4 1 2 3 2 3]);
 %
 % Vecteur des capacités max de chaque arc
-CAPMAX = [inf 1 4 5 2 3];
+CAPMAX = [inf 1 4 2 5 3];
 CAP = [0 0 0 0 0 0];
 %
 
@@ -46,7 +46,7 @@ while ~bloque % Tant que chemin possible
     chemin(1)= 2;
 	chemin = trouverChemin(2,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, chemin, 2);
 	%% 2. Mettre à jour les capicités des flots
-    if chemin == -1
+    if chemin(1) == -1
 		bloque = true;
 	else
 		alpha = inf;
@@ -59,26 +59,26 @@ while ~bloque % Tant que chemin possible
 			
 			trouve = false;
             if NSUC(noeud) ~= 0
-                prsuc = sum(NSUC(1:noeud-1)) + 1; % prsuc contient l'indice du 1er successeur de i dans SUC
+                prsuc = sum(NSUC(1:noeud-1)) + 1; % prsuc contient l'indice du 1er successeur du noeud dans SUC
                 k = prsuc;
                 while ~trouve && k<=prsuc + NSUC(noeud)-1  % on parcours les successeurs
                     j = SUC(k);
                     if (j == succ) % le flot est dans le bon sens
                         trouve = true;
-                        ind_cap(i) = noeud + k - prsuc;
-                        alpha = min(alpha, CAPMAX(ind_cap(i)));
+                        ind_cap(i) = k;
+                        alpha = min(alpha, CAPMAX(ind_cap(i)) - CAP(ind_cap(i)));
                     end
                     k = k+1;
                 end
             end
-            if NPRE(noeud) ~= 0
-                prpre = sum(NPRE(1:noeud-1)) + 1; % prpre contient l'indice du 1er predecesseur de i dans PRE
-                k = prpre;
-                while ~trouve && k<=prpre + NPRE(noeud)-1  % on parcours les predecesseurs
-                    j = PRE(k);
-                    if (j == succ) % le flot est dans le bon sens
+            if NSUC(succ) ~= 0
+                prsuc = sum(NSUC(1:succ-1)) + 1; % prpre contient l'indice du 1er successuer du successeur dans SUC
+                k = prsuc;
+                while ~trouve && k<=prsuc + NSUC(succ)-1  % on parcours les successeurs
+                    j = SUC(k);
+                    if (j == noeud) % le flot est dans le mauvais sens
                         trouve = true;
-                        ind_cap(i) = -(noeud + k - prpre);
+                        ind_cap(i) = -k;
                         alpha = min(alpha, CAP(-ind_cap(i)));
                     end
                     k = k+1;
@@ -86,8 +86,7 @@ while ~bloque % Tant que chemin possible
 			end
 		end
 		% On augmente les flots
-        disp(ind_cap);
-        disp(alpha);
+
         for i = 1:(length(chemin)-1)
             
             if ind_cap(i) >= 0
@@ -98,7 +97,7 @@ while ~bloque % Tant que chemin possible
             
         end
         CAP(1) = CAP(1) + alpha
-        %disp(CAP(1));
+        
     end
 end
 
--- Projet2/trouverChemin.m
+++ Projet2/trouverChemin.m
@@ -9,7 +9,7 @@ function[chemin] = trouverChemin(i,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, ch
         for k=prsuc:prsuc + NSUC(i)-1  % on parcours les successeurs
             j = SUC(k);
 			if ~MARQUE(j) && dernier~= 1
-				if CAP(i + k - prsuc) < CAPMAX(i + k - prsuc) % Si flot non saturé
+				if CAP(k) < CAPMAX(k) % Si flot non saturé
 					MARQUE(j)= true;
 					chemin(p)=j;
 					if j ~= 1
@@ -25,22 +25,26 @@ function[chemin] = trouverChemin(i,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX, ch
 			end
         end
     end
-    if i~=2 && chemin(size(chemin,2))~=1
+    if chemin(size(chemin,2))~=1
         if NPRE(i) ~= 0 % le nombre de predecesseurs de i est non nul
             prpre = sum(NPRE(1:i-1)) + 1; % prpre contient l'indice du 1er predecesseur de i dans SUC
             for k=prpre:prpre + NPRE(i)-1  % on parcours les predecesseur
                 j = PRE(k);
-                if ~MARQUE(j)
-                    if CAP(i + k - prpre) > 0 % Si flot non saturé
-                        MARQUE(j)= true;
-                        chemin(p)=j;
-                        if j ~= 1
-                            chemin = trouverChemin(j,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX,chemin, p+1);
-                            if chemin(size(chemin,2)) ~= 1 % Si on a pas atteint le bout
+                if ~MARQUE(j) && dernier~= 1
+                    prsuc = sum(NSUC(1:j-1)) + 1; % prsuc contient l'indice du 1er successeur de j dans SUC
+                    for q=prsuc:prsuc + NSUC(j)-1  % on parcours les successeurs
+                        if SUC(q) == i
+                            if CAP(q) > 0 % Si flot non saturé
+                                MARQUE(j)= true;
+                                chemin(p)=j;
+                            if j ~= 1
+                                chemin = trouverChemin(j,SUC, NSUC, PRE, NPRE, MARQUE, CAP, CAPMAX,chemin, p+1);                              
+                            end
+                            dernier = chemin(size(chemin,2));
+                            if dernier ~= 1 % Si on a pas atteint le bout
                                 MARQUE(j) = false;
                                 chemin = chemin(1:p-1);
-                                
-                            end
+                            end  
 						end
 					end
 				end
